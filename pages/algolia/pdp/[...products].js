aae7e6dbff315b3a2accd763056d8efecf467e10
Sebastian Contreras <sebastian.contreras@algolia.com>
2023-09-12 13:51:35 -0500

    Adding PDP,

--- pages/algolia/pdp/[...products].js
+++ pages/algolia/pdp/[...products].js
@@ -1,15 +1,18 @@
-export async function getServerSideProps({query}) {
-  const { products } = query;
-  const product = products.pop();
+import { searchClient, searchConfig } from "../../../lib/algoliaConfig";
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
-  const data = {
-    title: product,
-    description: 'Hello world'
-  }
+export async function getServerSideProps({ query }) {
+  const { products } = query;
+  const objectId = products.pop();
+
+  const index = searchClient.initIndex(searchConfig.recordsIndex);
+  const product = await index.getObject(objectId);
+
 
   return {
     props: {
-      data
+      hit: product
     }
   }
 }
@@ -19,12 +22,42 @@ export async function getServerSideProps({query}) {
  * @param {} param0
  * @returns
  */
-function ProductDetailPage({ data }) {
+function ProductDetailPage({ hit }) {
   return (
-    <div>
-      <h1>{data.title}</h1>
-      <p>{data.description}</p>
-    </div>
+    <>
+      <div className="pdp-hit">
+        <div className="pdp-hit-content">
+          <div className="pdp-note">
+            <p>Products data shoudln't come from Algolia. We are using <strong>getObject()</strong> for demo purposes. Don't use this approach in production.</p>
+          </div>
+          <div>
+            <h1>{hit.name}</h1>
+          </div>
+          <div className="pdp-hit-pictures">
+            <Carousel showArrows={true} showThumbs={true} axis={"horizontal"} centerMode="false" autoPlay={true} emulateTouch={true}>
+              {hit.image_urls.filter(u => u.length ).map((url, index) => (
+                <div key={index}>
+                  <img src={url} alt={`Image ${index}`} />
+                  <p className="legend">${hit.price.value}</p>
+                </div>
+              ))}
+            </Carousel>
+          </div>
+          <div className="pdp-hit-type">
+            <span>{hit.type}</span>
+          </div>
+          <p className='product-actions'>
+            <button className="conversion-btn"
+              onClick={(ev) => {
+                ev.preventDefault();
+                sendEvent('conversion', hit, 'Product Ordered');
+                ev.stopPropagation();
+              }}>Add to cart</button>
+          </p>
+        </div>
+      </div>
+    </>
+
   );
 }
 
